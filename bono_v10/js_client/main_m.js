9a858ffe566236f74f18a2f0e56bc5f8d82814b1
p22ju <p22ju@naver.com>
2015-03-02 11:25:00 +0900

    work_flag 알람창 오류 수정 (태블릿버전)

--- bono_v10/js_client/main_m.js
+++ bono_v10/js_client/main_m.js
@@ -370,8 +370,10 @@ $(document).ready(function() {
 					$(e.target).addClass("file_selected");
 
 					//alert("_work_flag:::: " + _work_flag);
-					if(work_flag == 1) {
+					if(_work_flag == 1) {
 						alert("다른 사용자가 수정중 입니다.");
+					} else {
+						alert("수정 가능한 파일입니다.");
 					}
 					make_editor(data, file_path, 0, _work_flag);
 
